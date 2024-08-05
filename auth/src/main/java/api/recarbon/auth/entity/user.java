0a553e48c05b3cc8b61279240c0847789e32c959
pramudia <pramudiaputra.iv@gmail.com>
2024-08-05 11:36:14 +0700

    init projects

---
+++ auth/src/main/java/api/recarbon/auth/entity/user.java
@@ -0,0 +1,36 @@
+package api.recarbon.auth.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user")
+public class user {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "role")
+    private String role;
+}
